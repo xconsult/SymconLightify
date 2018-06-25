dffe2128c432f48d07b1ce2eff334e9a866a613e
Mavericks <dreamtv@live.at>
2018-06-25 20:22:07 +0200

    Release 3.50 b15e007

--- libs/lightifyControl.php
+++ libs/lightifyControl.php
@@ -439,9 +439,9 @@ trait LightifyControl
                 if ($value == 1) {
                   if ($this->setStateOn($state)) {
                     if (false !== ($result = $lightifyConnect->setColorTemperature($uintUUID, $flag, $temperature))) {
-                      if ($this->itemLight && GetValue($this->InstanceID) != $value) {
-                        SetValue($this->InstanceID, $value);
-                      }
+                      //if ($this->itemLight && GetValue($this->InstanceID) != $value) {
+                        //SetValue($this->InstanceID, $value);
+                      //}
 
                       $this->sendData(classConstant::METHOD_RELOAD_LOCAL);
                       return true;
@@ -468,9 +468,9 @@ trait LightifyControl
                 if ($value == 1) {
                   if ($this->setStateOn($state)) {
                     if (false !== ($result = $lightifyConnect->setColor($uintUUID, $flag, $this->lightifyBase->HEX2RGB(classConstant::SCENE_PLANT_LIGHT)))) {
-                      if ($this->itemLight && GetValue($this->InstanceID) != $value) {
-                        SetValue($this->InstanceID, $value);
-                      }
+                      //if ($this->itemLight && GetValue($this->InstanceID) != $value) {
+                        //SetValue($this->InstanceID, $value);
+                      //}
 
                       $this->sendData(classConstant::METHOD_RELOAD_LOCAL);
                       return true;
